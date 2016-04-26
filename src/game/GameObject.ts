7af432d8be4289380ebfc23a77ae8e6c65155b4a
André Furchner <storkner@gmail.com>
2016-04-26 22:14:44 +0200

    GameObject ids and scene management

--- src/game/GameObject.ts
+++ src/game/GameObject.ts
@@ -1,10 +1,15 @@
 import {Component} from "./component/Component";
 
+let idCounter: number = -1;
+
 export class GameObject {
+
     private components: { [key:string]: Component; };
+    private id: number;
 
     constructor() {
         this.components = {};
+        this.id = idCounter++;
     }
 
     Add(component: Component) {
@@ -17,5 +22,13 @@ export class GameObject {
 
     Get<T extends Component>(type: {new(): T; }): T {
         return <T>this.components[type.prototype.constructor.name];
+    }
+
+    get ID (): number {
+        return this.id;
+    }
+
+    delete() {
+        // ok =(
     }
 }
--- src/game/Scene.ts
+++ src/game/Scene.ts
@@ -1,3 +1,18 @@
-export class Scene {
+import {GameObject} from "./GameObject";
 
+export class Scene {
+    private objects: { [key: number]: GameObject; };
+
+    constructor() {
+        this.objects = [];
+    }
+ 
+    Add(object: GameObject) {
+        this.objects[object.ID] = object;
+    }
+
+    Remove(object: GameObject) {
+        object.delete();
+        delete this.objects[object.ID];
+    }
 }
